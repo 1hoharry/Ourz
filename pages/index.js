f8fd8c210c042fde19511953bf18e11f7b9fa1ec
1hoharry <1hoharry@gmail.com>
2021-03-19 12:57:29 -0400

    Scaffold

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h3 className="font-bold text-2xl">Home</h3>;
+};
+
+export default Home;
---
+++ truffle-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  contracts_directory: './ethereum/contracts',
+  contracts_build_directory: './ethereum/abis',
+  migrations_directory: './ethereum/migrations',
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+  },
+  compilers: {
+    solc: {
+      version: ">=0.6.0 <0.8.0",
+    },
+  },
+};
